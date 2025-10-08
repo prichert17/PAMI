18f80c08d65f6c61e5876b6f49b3c0b597a6b9df
PaulR <prichert17@gmail.com>
2025-10-08 17:26:12 +0200

    marche avec interruption mais sans PID, juste commande

--- Projects/Test_asservissement/src/Moteur.cpp
+++ Projects/Test_asservissement/src/Moteur.cpp
@@ -1,34 +1,35 @@
 #include <Arduino.h>
 
-// Pins moteurs - STM32 L432KC
+// HARDWARE TIMER - STM32 L432KC
+// TIM6 : Timer contrôle PID
+HardwareTimer *tCtrl = new HardwareTimer(TIM6);
+// Note: TIM2 et TIM3 seront configurés plus tard pour encodeurs hardware
+
+// Pins moteurs
 const uint8_t motorPin1 = PA8;  // Moteur gauche sens 1
 const uint8_t motorPin2 = PA9;  // Moteur gauche sens 2  
 const uint8_t motorPin3 = PA10; // Moteur droit sens 1
 const uint8_t motorPin4 = PA11; // Moteur droit sens 2
 
-// Pins encodeurs
-const uint8_t encA1 = PA0; // Encodeur gauche A 
-const uint8_t encB1 = PA1; // Encodeur gauche B
-const uint8_t encA2 = PA7; // Encodeur droit A  
-const uint8_t encB2 = PA6; // Encodeur droit B
-
-// Variables encodeurs
+// Variables encodeurs temporaires (en attendant config hardware)
 volatile int32_t enc1Count = 0;
 volatile int32_t enc2Count = 0;
 volatile uint8_t lastStateEnc1 = 0;
 volatile uint8_t lastStateEnc2 = 0;
 
-// Variables vitesses
-float speed1 = 0.0;
-float speed2 = 0.0;
-
-// Variables commandes
+// Variables vitesses et commandes
+volatile float speed1 = 0.0;
+volatile float speed2 = 0.0;
 float cmd1 = 0.0;
 float cmd2 = 0.0;
 
-// ISR encodeur 1
+// Paramètres système
+const int pulsesPerRevolution = 1200;  // CPR encodeur
+const float Te = 0.001f;               // Période PID (1ms = 1kHz)
+
+// ISR encodeur temporaires (mode GPIO)
 void enc1_ISR() {
-  uint8_t newState = (digitalRead(encB1) << 1) | digitalRead(encA1);
+  uint8_t newState = (digitalRead(PA1) << 1) | digitalRead(PA0);
   uint8_t transition = (lastStateEnc1 << 2) | newState;
   
   switch(transition) {
@@ -38,9 +39,8 @@ void enc1_ISR() {
   lastStateEnc1 = newState;
 }
 
-// ISR encodeur 2
 void enc2_ISR() {
-  uint8_t newState = (digitalRead(encB2) << 1) | digitalRead(encA2);
+  uint8_t newState = (digitalRead(PA7) << 1) | digitalRead(PA6);
   uint8_t transition = (lastStateEnc2 << 2) | newState;
   
   switch(transition) {
@@ -48,6 +48,47 @@ void enc2_ISR() {
     case 0b0010: case 0b1011: case 0b1101: case 0b0100: enc2Count--; break;
   }
   lastStateEnc2 = newState;
+}
+
+// Configuration encodeurs (temporaire mode GPIO, à migrer vers hardware)
+void setupEncoders() {
+  // Configuration pins encodeurs en GPIO avec pullup
+  pinMode(PA0, INPUT_PULLUP); // Encodeur gauche A
+  pinMode(PA1, INPUT_PULLUP); // Encodeur gauche B
+  pinMode(PA6, INPUT_PULLUP); // Encodeur droit A
+  pinMode(PA7, INPUT_PULLUP); // Encodeur droit B
+  
+  // État initial
+  lastStateEnc1 = (digitalRead(PA1) << 1) | digitalRead(PA0);
+  lastStateEnc2 = (digitalRead(PA7) << 1) | digitalRead(PA6);
+  
+  // Interruptions GPIO (temporaire)
+  attachInterrupt(digitalPinToInterrupt(PA0), enc1_ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PA1), enc1_ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PA6), enc2_ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PA7), enc2_ISR, CHANGE);
+  
+  enc1Count = 0;
+  enc2Count = 0;
+  
+  Serial.println("Encodeurs configurés (mode GPIO temporaire)");
+}
+
+// Lecture delta encodeurs depuis dernière période PID
+inline int32_t readAndZeroEnc1() { 
+  noInterrupts();
+  int32_t c = enc1Count;
+  enc1Count = 0;
+  interrupts();
+  return c; 
+}
+
+inline int32_t readAndZeroEnc2() { 
+  noInterrupts();
+  int32_t c = enc2Count;
+  enc2Count = 0;
+  interrupts();
+  return c; 
 }
 
 // Fonction moteurs
@@ -74,8 +115,26 @@ void setMotors(float c1, float c2) {
   }
 }
 
+// ISR Timer contrôle PID (1kHz)
+void ctrlISR() {
+  // Lecture delta encodeurs depuis dernière période
+  int32_t delta1 = readAndZeroEnc1(); // Encodeur gauche
+  int32_t delta2 = readAndZeroEnc2(); // Encodeur droit
+  
+  // Conversion en vitesses (tr/s)
+  speed1 = delta1 / (pulsesPerRevolution * Te);
+  speed2 = delta2 / (pulsesPerRevolution * Te);
+  
+  // Commandes simples pour test
+  float cmd1_test = 0;   // Moteur gauche arrêté
+  float cmd2_test = 80;  // Moteur droit vitesse fixe
+  
+  setMotors(cmd1_test, cmd2_test);
+}
+
 void setup() {
   Serial.begin(115200);
+  Serial.println("STM32 Hardware Timer Encodeurs - Test");
   
   // Configuration pins moteurs
   pinMode(motorPin1, OUTPUT);
@@ -83,63 +142,46 @@ void setup() {
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
   
-  // Configuration pins encodeurs
-  pinMode(encA1, INPUT_PULLUP);
-  pinMode(encB1, INPUT_PULLUP);
-  pinMode(encA2, INPUT_PULLUP);
-  pinMode(encB2, INPUT_PULLUP);
+  // Configuration encodeurs hardware
+  setupEncoders();
   
-  // État initial encodeurs
-  lastStateEnc1 = (digitalRead(encB1) << 1) | digitalRead(encA1);
-  lastStateEnc2 = (digitalRead(encB2) << 1) | digitalRead(encA2);
+  // Configuration timer contrôle PID à 1kHz
+  tCtrl->setOverflow(1000, HERTZ_FORMAT);
+  tCtrl->attachInterrupt(ctrlISR);
+  tCtrl->resume();
   
-  // Interruptions encodeurs
-  attachInterrupt(digitalPinToInterrupt(encA1), enc1_ISR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(encB1), enc1_ISR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(encA2), enc2_ISR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(encB2), enc2_ISR, CHANGE);
+  Serial.println("Système prêt - Timer PID 1kHz actif");
   
-  Serial.println("STM32 Test Moteurs MINIMAL - Prêt");
-  
-  // Moteurs arrêtés
+  // Moteurs arrêtés au démarrage
   setMotors(0, 0);
 }
 
 void loop() {
-  static unsigned long lastTime = 0;
-  static int32_t lastEnc1 = 0;
-  static int32_t lastEnc2 = 0;
+  // Boucle principale - affichage debug seulement
+  static unsigned long lastDebug = 0;
   
-  if (millis() - lastTime > 100) { // Toutes les 100ms
+  if (millis() - lastDebug > 500) { // Debug toutes les 500ms
+    // Capture sécurisée des variables volatiles
+    noInterrupts();
+    float localSpeed1 = speed1;
+    float localSpeed2 = speed2;
+    interrupts();
     
-    // Calcul vitesses simples
+    // Affichage debug
     noInterrupts();
     int32_t currentEnc1 = enc1Count;
     int32_t currentEnc2 = enc2Count;
     interrupts();
     
-    int32_t delta1 = currentEnc1 - lastEnc1;
-    int32_t delta2 = currentEnc2 - lastEnc2;
-    
-    speed1 = delta1 * 10.0 / 1200.0; // tr/s approximatif
-    speed2 = delta2 * 10.0 / 1200.0;
-    
-    lastEnc1 = currentEnc1;
-    lastEnc2 = currentEnc2;
-    
-    // Test simple : moteur droit à vitesse fixe, gauche arrêté
-    setMotors(0, 100); // M1=0, M2=100
-    
-    // Debug
-    Serial.print("V1: "); Serial.print(speed1, 2);
-    Serial.print(" | V2: "); Serial.print(speed2, 2);
+    Serial.print("V1: "); Serial.print(localSpeed1, 3);
+    Serial.print(" | V2: "); Serial.print(localSpeed2, 3);
     Serial.print(" | C1: "); Serial.print(cmd1, 0);
     Serial.print(" | C2: "); Serial.print(cmd2, 0);
     Serial.print(" | E1: "); Serial.print(currentEnc1);
     Serial.print(" | E2: "); Serial.println(currentEnc2);
     
-    lastTime = millis();
+    lastDebug = millis();
   }
   
-  delay(10); // Délai pour stabilité
+  delay(10); // Évite une boucle trop agressive
 }
