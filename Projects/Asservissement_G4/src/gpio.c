e0089266ada4e5c56d9c89023eff8e9b0a921984
paulR <prichert17@gmail.com>
2026-01-19 13:45:00 +0100

    modification de l'allocation des pins (ajout contrôle led état), + modif auto pcb

--- Projects/Asservissement_G4/src/gpio.c
+++ Projects/Asservissement_G4/src/gpio.c
@@ -41,14 +41,16 @@
 */
 void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
   // Configuration de la LED LD2 sur PB8
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_8; // Tout le monde sur la même ligne !
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
